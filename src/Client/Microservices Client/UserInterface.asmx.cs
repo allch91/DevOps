45df43db72419f2eae5f3123a585c3f142eb7e9f
abner lopez <abner.lopezchavez@gmail.com>
2019-09-17 17:19:10 -0600

    Added unit test

--- src/Client/Microservices Client/UserInterface.asmx.cs
+++ src/Client/Microservices Client/UserInterface.asmx.cs
@@ -21,7 +21,8 @@ namespace Microservices_Client
         public string ServiceRequest(string user_id, string user_phone, int user_pos_init, int user_pos_end)
         {
 
-            return "Hi: " + user_id + ". Your service from: " + user_pos_init + " to: " + user_pos_end + " was received. You will get a message to: " + user_phone + ";" ;
+            UserRequest newRequest = new UserRequest();
+            return newRequest.SendRequest(user_id, user_phone, user_pos_init, user_pos_end);
         }
     }
 }
---
+++ src/Client/UnitTestClient/UnitTestRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestClient
+{
+    [TestClass]
+    public class UnitTestRequest
+    {
+        [TestMethod]
+        public void TestRequest()
+        {
+            Microservices_Client.UserRequest testRequest = new Microservices_Client.UserRequest();
+            string res = testRequest.SendRequest("user", "12345", 1, 2);
+            Assert.AreEqual(res, "Hi: user. Your service from: 1 to: 2 was received. You will get a message to: 12345;");
+        }
+    }
+}
